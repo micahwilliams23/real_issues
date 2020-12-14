15ad91788ce3bf296099ca95a38adbb876ae1439
micahwilliams23 <micahwilliams@college.harvard.edu>
2020-12-13 23:14:40 -1000

    added final trump section

--- site/app.js
+++ site/app.js
@@ -5,6 +5,7 @@ const trump_weeks = d3.csv('data/trump_averages.csv', function(x){
         pct: +x.pct,
         rm: +x.rm,
         n: +x.n,
+        t: +x.t,
         date: Date(x.date)
     };
 })
@@ -123,6 +124,23 @@ function showLines1(){
         .duration(500)
         .attr('stroke-opacity', 1);
 
+    container.selectAll('.line2')
+        .transition()
+        .duration(500)
+        .attr('opacity', 0);
+
+    container.selectAll('.yaxis2')
+        .transition()
+        .duration(500)
+        .attr('opacity', 0);
+
+    // change y grid
+    container.selectAll('.ygrid')
+        .transition()
+        .duration(500)
+        .attr('y1', d => yScale(d))
+        .attr('y2', d => yScale(d))
+
     setTimeout(
         function(){
             container.selectAll('.point')
@@ -130,12 +148,24 @@ function showLines1(){
                 .duration(500)
                 .attr('opacity', 0)
                 .delay(data => delayScale(data.week));
+
+            container.selectAll('.yaxis1')
+                .transition()
+                .duration(500)
+                .attr('opacity', 1);
             },
             500
     );
 };
 
 function showLines2(){
+
+    showContainer()
+
+    svgbase
+        .transition()
+        .duration(500)
+        .attr('opacity', 1);
 
     container.selectAll('.line1')
         .transition()
@@ -147,20 +177,31 @@ function showLines2(){
         .duration(500)
         .attr('opacity', 0);
 
+    container.selectAll('.totalCircles')
+        .transition()
+        .duration(500)
+        .attr('r', 1)
+
+    container.selectAll('.totals')
+        .transition()
+        .duration(500)
+        .attr('opacity', 0)
+
     container.select('#plot-title')
         .transition()
         .duration(500)
         .attr('fill-opacity', '0')
         .transition()
         .attr('fill-opacity', '1')
-        .text('Number CNN and Fox News Titles Containing \'Trump\' by month')
+        .text('Total Number of CNN and Fox News Titles Containing \'Trump\' since Sep. 2015')
 
     setTimeout(
         function(){
             container.selectAll('.line2')
                 .transition()
                 .duration(500)
-                .attr('stroke-opacity', 1);
+                .attr('opacity', 1)
+                .attr('fill-opacity', 0.5);
 
             container.selectAll('.yaxis2')
                 .transition()
@@ -179,6 +220,29 @@ function hideContainer(){
 
     hidePoints()
 };
+
+function showCircles(){
+    
+    container.select('.totals')
+        .transition()
+        .duration(500)
+        .attr('opacity', 1);
+
+    container.selectAll('.totalCircles')
+        .transition()
+        .duration(500)
+        .attr('r', data => radiusScale(data.value));
+
+    //  hide everything else
+    container.selectAll('.line2')
+        .transition()
+        .duration(500)
+        .attr('opacity', 0);
+    svgbase
+        .transition()
+        .duration(500)
+        .attr('opacity', 0);
+}
 
 function noTransition(){};
 
@@ -214,7 +278,8 @@ dispatch.on('active', function(index){
         showPoints,
         toMean,
         showLines1,
-        showLines2
+        showLines2,
+        showCircles
     ]
 
     transitions[index]();
@@ -246,6 +311,7 @@ function position(){
 d3.select('#sections')
     .on('scroll.scroller', position);
 
+//  add scales
 const xTicks = [2016, 2017, 2018, 2019, 2020, 2021];
 const xScale = d3.scaleLinear()
     .domain([2015.7, 2021])
@@ -255,33 +321,42 @@ const yTicks = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6];
 const yScale = d3.scaleLinear()
     .domain([0, 0.6])
     .range([height, margin.top]);
+const yScale2 = d3.scaleLinear()
+    .domain([0, 30000])
+    .range([height, margin.top]);
 
 const colorScale = d3.scaleOrdinal()
     .domain(['CNN','Fox News'])
-    .range(['#377eb8','#e41a1c']);
+    .range(['#75c0fd','#e41a1c']);
 
-// create scale to calculate delay time
 const delayScale = d3.scaleLinear()
     .domain([2015.692, 2020.942])
     .range([0, 1000]);
+
+const radiusScale = d3.scaleSqrt()
+        .domain([0, 143425])
+        .range([0, 200])
 
 const container = d3.select('svg')
     .attr('opacity', 0)
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom);
 
+const svgbase = container.append('g')
+    .classed('svgbase', true)
+
 // add y axis
-container.append('g')
+svgbase.append('g')
     .classed('yaxis1', true)
     .call(d3.axisLeft(yScale).ticks(5).tickFormat(d => d * 100 + '%'))
     .attr('transform', 'translate('+ margin.left +',0)');
 
 // add y grid
-container.selectAll('.grid')
+svgbase.selectAll('.ygrid')
     .data(yTicks)
     .enter()
     .append('line')
-    .classed('.grid', true)
+    .classed('.ygrid', true)
     .attr('x1', margin.left)
     .attr('x2', window.innerWidth * 0.65)
     .attr('y1', d => yScale(d))
@@ -290,16 +365,16 @@ container.selectAll('.grid')
     .attr('stroke', '#444')
         
 // add x axis
-container.append('g')
+svgbase.append('g')
     .call(d3.axisBottom(xScale).ticks(5).tickValues(xTicks).tickFormat(d => d))
     .attr('transform', 'translate(' + margin.left + ',' + height +')');
 
 // add x grid
-container.selectAll('.grid')
+svgbase.selectAll('.xgrid')
     .data(xTicks)
     .enter()
     .append('line')
-    .classed('.grid', true)
+    .classed('.xgrid', true)
     .attr('y1', height)
     .attr('y2', 0)
     .attr('x1', d => xScale(d) + margin.left)
@@ -308,7 +383,7 @@ container.selectAll('.grid')
     .attr('stroke', '#444')
     
 // add plot title
-container.append('text')
+svgbase.append('text')
     .attr('id', 'plot-title')
     .attr('fill', 'white')
     .attr('x', 0)
@@ -333,7 +408,7 @@ trump_weeks.then(function(d){
         .key(d => d.network)
         .entries(d);
     
-    // add lines
+    // add first set of lines
     container.selectAll('.line1')
         .data(nested_d)
         .enter()
@@ -349,19 +424,6 @@ trump_weeks.then(function(d){
             .y(d => yScale(d.rm))
             (d.values)
         });
-})
-
-// add second y scale
-const yScale2 = d3.scaleLinear()
-        .domain([0, 1420])
-        .range([height, margin.top])
-
-trump_months.then(function(d){
-
-    // nest data
-    var nested_d = d3.nest()
-        .key(d => d.network)
-        .entries(d);
 
     // add second set of lines
     container.selectAll('.line2')
@@ -369,21 +431,79 @@ trump_months.then(function(d){
         .enter()
         .append('path')
         .classed('line2', true)
-        .attr('fill', 'none')
-        .attr('stroke-opacity', 0)
+        .attr('fill', data => colorScale(data.key))
+        .attr('opacity', 0)
         .attr('stroke', data => colorScale(data.key))
         .attr('stroke-width', 3)
         .attr('d', function(d){
-            return d3.line()
-            .x(d => margin.left + xScale(d.month))
-            .y(d => yScale2(d.n))
+            return d3.area()
+            .x(d => margin.left + xScale(d.week))
+            .y1(d => yScale2(d.t))
+            .y0(yScale2(0))
             (d.values)
         });
 
     // add second y axis
-    container.append('g')
+    svgbase.append('g')
         .classed('yaxis2', true)
-        .call(d3.axisLeft(yScale2).ticks(5))
+        .call(d3.axisLeft(yScale2).ticks(5).tickFormat(d => d/1000 + 'k'))
         .attr('transform', 'translate('+ margin.left +',0)')
-        .attr('opacity', '1');
-});
+        .attr('opacity', 0);
+    
+    const xBands = d3.scaleBand()
+        .domain(['CNN', 'Fox News'])
+        .range([margin.left + 100, width - 100])
+
+    // add network totals data
+    var networkTotals = [
+        {network: 'CNN', value: '143425', f0: 0.5},
+        {network: 'CNN', value: 28107, f0: 1},
+        {network: 'Fox News', value: 5237, f0: 0.5},
+        {network: 'Fox News', value: 1592, f0: 1},
+    ]
+
+    // add network totals circles
+    container.append('g')
+        .classed('totals', true)
+        .selectAll('.totalCircles')
+        .data(networkTotals)
+        .enter()
+        .append('circle')
+        .classed('totalCircles', true)
+        .attr('r', 1)
+        .attr('cy', function(data){
+            if (data.value == 1592) {
+                return height / 2 + radiusScale(5237) - radiusScale(1592);
+            } else if (data.value == 28107) {
+                return height / 2 + radiusScale(143425) - radiusScale(28107);
+            } else return height / 2;
+        })
+        .attr('cx', data => width / 4 + xBands(data.network))
+        .attr('stroke-width', 3)
+        .attr('stroke', data => colorScale(data.network))
+        .attr('fill-opacity', data => data.f0)
+        .attr('fill', data => colorScale(data.network)); 
+
+    container.select('.totals')
+        .selectAll('.totalText')
+        .data(networkTotals)
+        .enter()
+        .append('text')
+        .classed('totalText', true)
+        .attr('x', data => width / 4 + xBands(data.network))
+        .attr('y', function(data){
+            if (data.value == 1592) {
+                return height / 2 + radiusScale(1592);
+            } else if (data.value == 28107) {
+                return height / 2 + radiusScale(28107) + margin.bottom;
+            } else {return height / 2};
+        })
+        .text(data => data.value)
+        .attr('fill', 'white')
+        .attr('text-anchor', 'middle')
+        .attr('font-weight', 'bolder')
+        .attr('font-family', 'Helvetica')
+
+    container.select('.totals')
+        .attr('opacity', 0)
+})
